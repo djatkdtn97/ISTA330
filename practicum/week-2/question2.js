97189299bc054f45980fa81fe2955e82b1909174
Brian Um <djatkdtn97@email.arizona.edu>
2021-12-10 04:22:16 -0700

    master

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -9,43 +9,24 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
          and therfore each of the three kids will get 8 cookies.
  */
 
- var canGetEqualCookies = function(cookies, extraCookies) {
-        //  var largest = 0;
-        //  for (i = 0; i < largest; i++) {
-        //          if (cookies[i]>largest) {
-        //                  var largest = array[i];
-        //         }
-        // }
-        //  var cloneArray = cookies.slice();
-        //  var i = largest;
-        //  cloneArray.splice(i,1);
-        // var cloneArray = cookies.slice();
-        // const removeMax = numbers => {
-        //         const max = Math.max(...numbers)
-        //         return numbers.filter(number => number !== max)
-        // }
-        // removeMax(cloneArray.cookies)
-        // const sum = cloneArray.cookies.reduce(add,0);
-        // function add (accumulator, a) {
-        //         return accumulator + a;
-        // }
-        // if (sum == extraCookies) {
-        //         return true;
-        // } else {
-        //         return false;
-        // }
-        // const newCookie = [...cookies];
-        // const MaxNum = Math.max(cookies);
-        // newCookie.splice(MaxNum)
-        // let sum = 0;
-        // for (let i = 0; i <newCookie.length; i++) {
-        //         sum += newCookie[i];
-        // }
-        // var subtractNum = MaxNum - sum;
-        // if (subtractNum === extraCookies) {
-        //         print('true')
-        // }
-
-
-
- }
+         var canGetEqualCookies = function(cookies, extraCookies) {
+              let cookienum = extraCookies;
+              let child_largestCookieNum = 0;
+              for(var i=0; i<cookies.length; i++){
+                      if(cookies[i]>child_largestCookieNum){
+                              child_largestCookieNum=cookies[i];
+                      }
+              }
+              for(var i=0; i<cookies.length; i++){
+                      if(child_largestCookieNum>cookies[i]){
+                              let need_cookie = child_largestCookieNum-cookies[i];
+                              if(cookienum>=need_cookie){
+                                      cookienum-=need_cookie;
+                              }
+                              else{
+                                      return false;
+                              }
+                      }
+              }
+              return true;
+       };
